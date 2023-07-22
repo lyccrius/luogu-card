ac788dc1a7f7690a03811167a94b53f01083d24c
Lyccrius <lyccrius@gmail.com>
2023-07-22 08:35:53 +0800

    delete 卡片生成时间

--- src/common.js
+++ src/common.js
@@ -72,7 +72,6 @@ class Card {
                 <g transform="translate(${this.paddingX}, ${this.hideTitle ? this.paddingY : this.paddingY + this.titleHeight})">
                     ${this.body}
                 </g>
-                <g transform="translate(${this.paddingX}, ${cardSize.height - this.paddingY})"><text x="0" y="15" class="about-text-grey">卡片生成时间：${timeStr}</text></g>
             </svg>`;
     }
 }
